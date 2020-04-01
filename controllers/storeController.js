8e23abe1bf6fd0e8a140eebd5227aaad2378c58c
josemariodev <josemariodev@gmail.com>
2020-04-01 18:58:31 -0500

    :sparkles: Complete through module 5
    Geolocation. saving Lat/Lng, geocoding using Google Maps, Data visualization updates

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -37,6 +37,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  // set location data to be a point
+  req.body.location.type = 'Point';
   // find and update store
   const store = await await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true,
