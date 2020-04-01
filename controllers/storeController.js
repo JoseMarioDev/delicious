2e855396e0afc8246fa6a6714a498161c362755a
josemariodev <josemariodev@gmail.com>
2020-03-31 23:54:17 -0500

    :sparkles: Complete through module 4
    control flow - async/await, flash msgs, querying db for stores, create/edit flow

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -8,12 +8,41 @@ exports.homePage = (req, res) => {
   res.render('index');
 };
 
+// returns view of addstore page
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
+// actually creates store
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await new Store(req.body).save();
+  req.flash('success', `successfully created ${store.name}.  Please leave a review`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+// query db for all getStores
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'stores', stores });
+};
+
+// edit store
+exports.editStore = async (req, res) => {
+  // 1. find store given ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are owner of store
+  // todo
+  // 3. render out edit form to update store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update store
+  const store = await await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  }).exec();
+  req.flash('success', `Successfully updated ${store.name}.  <a href="/stores/${store.slug}">View Store -></a>`);
+  // redirect and confirm success
+  res.redirect(`/stores/${store._id}/edit`);
 };
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,19 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-// Do work here
+// get stores, home and /stores -> same location
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 
-router.get('/', storeController.homePage);
+// get addstore page
 router.get('/add', storeController.addStore);
+
+// route for when you submit new store
 router.post('/add', catchErrors(storeController.createStore));
+// editing current store
+router.post('/add/:id', catchErrors(storeController.updateStore));
+
+// route for edit store
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
