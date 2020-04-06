a54a27ce70077fc3990051ad710973cf1779429e
josemariodev <josemariodev@gmail.com>
2020-04-05 22:01:52 -0500

    :sparkles: Complete through module 13 - advanced relationships & Aggregations
    adding Reviews data model, advanced relationship population, advanced aggreg

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -103,7 +103,7 @@ exports.updateStore = async (req, res) => {
     `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`
   );
   res.redirect(`/stores/${store._id}/edit`);
-  // Redriect them the store and tell them it worked
+  // redirect them the store and tell them it worked
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // get stores, home and /stores -> same location
@@ -87,5 +88,9 @@ router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 // view all hearts page
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
+
+router.get('/top', catchErrors(storeController.getTopStores));
 
 module.exports = router;
